baef8bde9a59adc3e35e945028a1e34bb85f29bc
Anesa Jakupi <anesa.jakupi@ibu.edu.mk>
2026-03-23 19:42:15 +0100

    Add files via upload

---
+++ week1/pset1/cash.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void)
+{
+    int cents;
+    do
+    {
+        printf("Change owed: ");
+        scanf("%d", &cents);
+    }
+    while (cents < 0);
+    int coins=0;
+    while (cents >= 25)
+    {
+        cents = cents - 25;
+        coins++;
+    }
+    while (cents >= 10)
+    {
+        cents = cents - 10;
+        coins++;
+    }
+    while (cents >= 5)
+    {
+        cents = cents - 5;
+        coins++;
+    }
+    while (cents >= 1)
+    {
+        cents = cents - 1;
+        coins++;
+    }
+    printf("%d\n", coins);
+}
